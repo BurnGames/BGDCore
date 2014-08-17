8fe61bba927f2a012f2533751049af6c91a88d82
PaulBGD <paul@paulbgd.me>
2014-08-17 15:25:11 -0500

    Testing jenkins still

--- src/main/java/me/paulbgd/bgdcore/BGDCore.java
+++ src/main/java/me/paulbgd/bgdcore/BGDCore.java
@@ -7,6 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import lombok.Getter;
 import me.paulbgd.bgdcore.commands.def.ReloadConfigCommand;
+import me.paulbgd.bgdcore.commands.def.ZipTestCommand;
 import me.paulbgd.bgdcore.configuration.ConfigurationFile;
 import me.paulbgd.bgdcore.configuration.CoreConfiguration;
 import me.paulbgd.bgdcore.nms.NMSManager;
@@ -35,7 +36,7 @@ public class BGDCore extends JavaPlugin {
 
         // register our own commands
         new ReloadConfigCommand(this);
-        // new ZipTestCommand();
+        new ZipTestCommand();
 
         // register our own configuration
         registerConfiguration(new CoreConfiguration(new File(getDataFolder(), "config.json")));
